9312cc350c095066105738a2ab1549c1908a00de
khiarimarwa <khiari.marwa@gmail.com>
2026-01-04 14:13:02 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ st.title ("Marwa Site")
 st.write ("Bienvenue sur mon site web")
 
 data = {"Nom" : ["Alice", "Bob", "Marwa"],
-        {"Age" : [10,25,26]
+        "Age" : [10,25,26] }
         df = tab.DataFrame(data)
 
 st.dataframe(df)
