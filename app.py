5858c5dc87ec4bea516684f82f47552138bb5f2f
khiarimarwa <khiari.marwa@gmail.com>
2026-01-04 14:12:01 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 import streamlit as st 
-import model as m 
+import pandas as tab
 st.title ("Marwa Site")
 st.write ("Bienvenue sur mon site web")
-m.write("hello")
+
+data = {"Nom" : ["Alice", "Bob", "Marwa"],
+        {"Age" : [10,25,26]
+        df = tab.DataFrame(data)
+
+st.dataframe(df)
